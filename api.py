70280ffade88ff6f1b570caf49bcf1c8eded2d9a
Rudy Laprade <rlaprade@berkeley.edu>
2015-04-13 17:49:41 -0700

    team separated ult cd lookup

--- api.py
+++ api.py
@@ -16,16 +16,31 @@ class RiotAPI(object):
     def get_champions_in_match(self, name):
         """Returns a list of the champions in the current game with summoner of given name"""
         lst = []
-        try:
-            participant_data = self._current_match_participants(name)
-        except ValueError as e:
-            if e.message == "No JSON object could be decoded":
-                print("No match found for summoner {name}".format(name=name))
-                return
+        participant_data = self._current_match_participants(name)
         for p in participant_data:
             champion = self._get_champion_name(p["championId"])
             lst.append(str(champion))
         return lst
+        
+    def get_champions_in_match_by_team(self, name):
+        """Returns two lists of the champions, one for each team,
+        for the current game of the specified summoner (name)
+        """
+        allies = []
+        enemies = []
+        summoner_id = self._get_id_by_summoner_name(name)
+        participant_data = self._current_match_participants(name)
+        for p in participant_data:
+            if p["summonerId"] == summoner_id:
+                allies_id = p["teamId"]
+        for p in participant_data:
+            champion = self._get_champion_name(p["championId"])
+            team_id = p["teamId"]
+            if team_id == allies_id:
+                allies.append(str(champion))
+            else:
+                enemies.append(str(champion))
+        return allies, enemies
         
     def get_ult_cooldowns(self, champion):
         """Returns a tuple of the ultimate cooldown values for given champion"""
@@ -34,13 +49,17 @@ class RiotAPI(object):
         
     def get_ult_cooldowns_for_match(self, name):
         """Returns a list of pairs of champion name and ultimate cooldowns for current match"""
-        champions_ingame = self.get_champions_in_match(name)
-        return [(champion, self.get_ult_cooldowns(champion)) for champion in champions_ingame]
-
-    def print_ult_cooldwons_for_match(self, name):
-        data = self.get_ult_cooldowns_for_match(name)
-        for line in data:
-            print("{champion}:  {cds}".format(champion=line[0], cds=line[1]))
+        allies, enemies = self.get_champions_in_match_by_team(name)
+        get_cds  = lambda champions: [(champion, self.get_ult_cooldowns(champion))
+                                        for champion in champions]
+        return get_cds(allies), get_cds(enemies)
+        
+    def print_ult_cooldowns_for_match(self, name):
+        allies, enemies = self.get_ult_cooldowns_for_match(name)
+        print("Ally team:")
+        self._print_ult_cooldowns(allies)
+        print("\nEnemy team:")
+        self._print_ult_cooldowns(enemies)
         
     def _request(self, api_url, params={}):
         args = {'api_key':  self.api_key}
@@ -52,7 +71,8 @@ class RiotAPI(object):
         return response.json()
         
     def _get_summoner_by_name(self, name):
-        api_url = URL["summoner_by_name"].format(region=self.region, version=API_VERSIONS["summoner"], names=name)
+        api_url = URL["summoner_by_name"].format(region=self.region,
+                                                version=API_VERSIONS["summoner"], names=name)
         return self._request(api_url)
         
     def _get_id_by_summoner_name(self, name):    
@@ -65,19 +85,21 @@ class RiotAPI(object):
     def _get_champion_name(self, id):
         return self.id_map[id]
         
-    def _champion_list(self):
-        api_url = URL["champion_list"].format(region=self.region, version=API_VERSIONS["static-data"])
-        return self._request(api_url)
-        
     def _get_champion_data(self, champion):
         id = self._get_champion_id(champion)
-        api_url = URL["champion"].format(region=self.region, version=API_VERSIONS["static-data"], id=id)
+        api_url = URL["champion"].format(region=self.region, 
+                                        version=API_VERSIONS["static-data"], id=id)
         return self._request(api_url)
         
     def _get_champion_ability_data(self, champion):
         """Returns a list of ability data objects (type dict)"""
         data = self._get_champion_data(champion)
         return data["spells"]
+
+    def _champion_list(self):
+        api_url = URL["champion_list"].format(region=self.region, 
+                                            version=API_VERSIONS["static-data"])
+        return self._request(api_url)
         
     def _champion_name_to_id_map(self):
         name_map = {}
@@ -93,22 +115,36 @@ class RiotAPI(object):
         
     def _current_match_by_id(self, id):
         """Returns current match data for given summoner id"""
-        api_url = URL["curret_match"].format(platformId=PLATFORM_ID[self.region], summonerId=id)
+        api_url = URL["curret_match"].format(platformId=PLATFORM_ID[self.region],
+                                            summonerId=id)
         return self._request(api_url)
         
     def _current_match(self, name):
         """Returns current match data for given summoner name"""
         id = self._get_id_by_summoner_name(name)
-        return self._current_match_by_id(id)
+        try:
+            return self._current_match_by_id(id)
+        except ValueError as e:
+            if e.message == "No JSON object could be decoded":
+                raise InputError("No match found for summoner {name}".format(name=name))
         
     def _current_match_participants(self, name):
-        """Returns a list of participant data dicts for the current match of given summoner name"""
+        """Returns a list of participant data dicts 
+        for the current match of specified summoner (name)
+        """
         return self._current_match(name)["participants"]
+        
+    def _print_ult_cooldowns(self, cd_info):        
+        for line in cd_info:
+            print("{champion}:  {cds}".format(champion=line[0], cds=line[1]))
+            
+class InputError(Exception):
+    pass
         
 def main():
     requests.packages.urllib3.disable_warnings()  #Suppresses Insecure Connection warnings
     api = RiotAPI(my_key)
-    api.print_ult_cooldwons_for_match("DyrudeJstorm RMX")
+    api.print_ult_cooldowns_for_match("DyrudeJstorm RMX")
     
 if __name__ == "__main__":
     main()
